410c56ddc3ba317f2670636830c2a0fc93e1d49b
alehad <64356216+alehad@users.noreply.github.com>
2020-05-09 15:34:05 +0100

    initial ws soap commit

---
+++ IPServiceSoapClient/src/alehad/ws/soap/IPServiceSoapClient.java
@@ -0,0 +1,10 @@
+package alehad.ws.soap;
+
+public class IPServiceSoapClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
