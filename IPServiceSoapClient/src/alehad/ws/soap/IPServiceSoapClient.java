cc4798c6e66a2eea78378274cc3bc01ce40802a6
alehad <64356216+alehad@users.noreply.github.com>
2020-05-09 16:02:55 +0100

    commit GeoIPService source and client files

--- IPServiceSoapClient/src/alehad/ws/soap/IPServiceSoapClient.java
+++ IPServiceSoapClient/src/alehad/ws/soap/IPServiceSoapClient.java
@@ -1,10 +1,25 @@
 package alehad.ws.soap;
 
+import net.webservicex.*;
+
 public class IPServiceSoapClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		if (args.length != 1) {
+			System.out.println("Please enter IP address");
+			return;
+		}
+		
+		String ipAddress = args[0];
+		
+		GeoIPService ipService = new GeoIPService();
+		
+		GeoIPServiceSoap soapPort = ipService.getGeoIPServiceSoap();
+		
+		GeoIP geoIP = soapPort.getGeoIP(ipAddress);
+
+		System.out.println(geoIP.getCountryName());
 	}
 
 }
